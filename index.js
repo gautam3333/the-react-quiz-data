9c777421884fe5167d2739ff6a232213cd4d53e0
Gautam Sarkar <gautam.sarkar3333@gmail.com>
2023-08-09 11:26:20 +0530

    create json server

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('questions.json');
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
